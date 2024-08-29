674328f8c6010a3a5aef2ea5d74084d4f5277268
Daniel Grounin <69406706+Daniel-Grounin@users.noreply.github.com>
2024-08-30 02:20:04 +0300

    Time: 8 ms (26.21%), Space: 10.6 MB (9.45%) - LeetHub

---
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int temp_max;
+        int max = 0;
+        for(vector<int> account : accounts){
+            temp_max = 0;
+            for(int element : account){
+                temp_max += element;                
+            }
+            if(temp_max > max){
+                max = temp_max;
+            }
+            cout << endl;
+
+        }
+        return max;
+    }
+};
