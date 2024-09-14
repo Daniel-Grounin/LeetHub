ce2f175dfc0aaad5ffccd3aa6f0cdf1c04baa703
Daniel Grounin <69406706+Daniel-Grounin@users.noreply.github.com>
2024-09-14 23:00:19 +0300

    Time: 0 ms (100%), Space: 12 MB (7.23%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -12,15 +12,31 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if(root != NULL){
-            invertTree(root->left);
-            invertTree(root->right);
-        
-            //Swap
-            TreeNode* temp = root->right;
-            root->right = root->left;
-            root->left = temp;
+        if (root == NULL) {
+            return NULL;
         }
+        
+        queue<TreeNode*> queue;
+        queue.push(root);
+        
+        while (!queue.empty()) {
+            TreeNode* current = queue.front();
+            queue.pop();
+            
+            // Swap the children
+            TreeNode* temp = current->left;
+            current->left = current->right;
+            current->right = temp;
+            
+            // Push non-null children to the queue
+            if (current->left != NULL) {
+                queue.push(current->left);
+            }
+            if (current->right != NULL) {
+                queue.push(current->right);
+            }
+        }
+        
         return root;
     }
 };
